3ce215b4c80c918f0319e46acc21dce1808ccfcd
Stephanie Villalobos <svillalobos2027@gmail.com>
2026-01-27 10:25:34 -0800

    Initial Commit

---
+++ MathTutor/MathTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathTutorApp.swift
+//  MathTutor
+//
+//  Created by VILLALOBOS, STEPHANIE on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct MathTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
